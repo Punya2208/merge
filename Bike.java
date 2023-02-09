5a3225d2f3aefabf6caf20a1d6d4b0d0c500d23f
supriya0311 <supriyapriyarl01@gmail.com>
2023-02-09 18:52:09 +0530

    mesg

---
+++ Bike.java
@@ -0,0 +1,19 @@
+class Bike
+{
+	int Bike_cost;
+    static String Bike_brand="Jupiter";
+	String Bike_colour;
+	public static void main(String[]args)  
+	{
+		System.out.println("Bike brand is "+Bike_brand);
+		{
+		Bike s1=new Bike();
+		s1.Bike_cost=50000;
+		System.out.println("Bike cost is"+s1.Bike_cost);
+		Bike s2=new Bike();
+	    s2.Bike_colour=("white");
+        System.out.println("Bike type is"+s2.Bike_colour);
+
+	}
+}
+}
